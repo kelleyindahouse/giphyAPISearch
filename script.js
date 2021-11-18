e5d605979c8a5d35514ead3ffd53b38fd05936bf
Nathan Montelli <nathanmontelli@gmail.com>
2021-11-18 14:49:16 -0800

    added randomly generated gifs

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 
 const test = () => {
-    axios.get("https://api.giphy.com/v1/gifs/search?api_key=LkjgCSv5Afgm1PFuRjJZ28InVp1uCBNQ&q=dog").then(res => {
-        console.log(res);
+    axios.get("https://api.giphy.com/v1/gifs/search?api_key=LkjgCSv5Afgm1PFuRjJZ28InVp1uCBNQ&q=dog&limit=10").then(res => {
+        const gifs = res.data.data
+        let random = Math.floor(Math.random() * gifs.length)
+        console.log(gifs)
+        console.log(random)
+        document.getElementById('showGifs').
+        innerHTML= `
+        <img src ='${gifs[random].images.original.url}'></img>
+        `
     });
-    alert("check console for api test");
+     
 }
-
+// .catch (err => console.log(err))
 test();
